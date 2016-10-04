1509efa4a65ed606840f5258129e334c34d6eac0
Tom <Thomas.mann@kearsarge.org>
2016-10-04 11:58:37 -0400

    start

---
+++ input.py
@@ -0,0 +1,78 @@
+def isSwear(word):
+    swearList = ["poop",
+                 "pee",
+                 "ass",
+                 "dick",
+                 "fuck",
+                 "shit",
+                 "cunt",
+                 "bitch",
+                 "slut",
+                 "slutty",
+                 "nigger",
+                 "penis",
+                 "tits",
+                 "boobs",
+                 "cock",
+                 "anus"]
+    if word in swearList:
+        return True
+    else:
+        return False
+
+def getMenuOption():
+    goodInput = False
+    goodResponses = ["1",
+                     "2",
+                     "3",
+                     "q"]
+    while not goodInput:
+        response = raw_input("Make a selection: ")
+        if response.lower() in goodResponses:
+            goodInput = True
+        else:
+            print "Please make a valid selection!"
+    return response.lower()
+
+def getWord(prompt):
+    goodInput = False
+    while not goodInput:
+        response = raw_input(prompt)
+        goodInput = True
+        if isSwear(response):
+            goodInput = False
+            print "Don't use that kind of language with me!"
+        return response
+
+def getNumber(prompt):
+    goodInput = False
+    numbers = "0123456789."
+    while not goodInput:
+        response = raw_input(prompt)
+        goodInput = True
+        for character in response:
+            if character not in numbers:
+                goodInput = False
+                print "Numbers only please!"
+    return response
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
