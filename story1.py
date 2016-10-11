3d40c8def6dc30fcd9ead1f3e962684f09b1bf05
matt <matthew.disalvo@kearsarge.org>
2016-10-11 11:55:03 -0400

    Added more

--- story1.py
+++ story1.py
@@ -11,22 +11,21 @@ def story():
     text += " out."
     text += " I looked for my friend " + Name 
     text += " s/he was always later so I " + Noun 
-    text += " it was " + time " when s/he arived " 
-    text += " I asked " + Name " what took him/her " + Noun
-    text += " s/he said " + Name " was talking to me about" + pronoun
+    text += " it was " + time + " when s/he arived "
+    text += " I asked " + Name + " what took him/her " + Noun
+    text += " s/he said " + Name + " was talking to me about" + pronoun
     text += " I looked at her/him and thought of " + Noun
-    text += " but then " + name " walked up to me and asked me if I was ok I said" + adajetive
-    text += " I then sais we shoud go to " + restront Name " for lunch "
-    text += " I know how much " + Name " loved there" + Food
-    text += " we starwd walking to " + restront Name 
+    text += " but then " + name + " walked up to me and asked me if I was ok I said" + adajetive
+    text += " I then sais we shoud go to " + restrontName + " for lunch "
+    text += " I know how much " + Name + " loved there" + Food
+    text += " we starwd walking to " + restrontName 
     text += " we stard taking about " + Noun
     text += " after some time past we made it to the restront I looked at my wach to see it wat " + time 
     text += " I then remaber that I had to pick up my sister " + Name 
     text += " she was just getting back from " + city
     text += " her plane was landing at " + time 
     text += " I see that I have time to eat with my friend " + Name 
-    text += " 
     
-    +
+    
     return text
 
--- story2.py
+++ story2.py
@@ -1,1 +1,15 @@
+from input import *
 
+#Written by Matthew 
+def story():
+	location1 = getWorld("Enter a Location: ")
+	temperaturel = getNumber ("Enter a Number: ")
+	
+	text = ""
+	text =+ "You see a clown walking on the road."
+
+	
+	
+	
+	
+	return text 
