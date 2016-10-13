fe3053206c5aa755bb2d3e3645e028c6dbe72c36
Felicia Merrill <Felicia.Merrill@kearsarge.org>
2016-10-13 10:35:38 -0400

    add to story one

--- story1.py
+++ story1.py
@@ -2,44 +2,52 @@ from input import *
 
 #Written by Mr. Spooner
 def story():
-    location1 = getWord("Enter a location: ")
-    temperature1 = getNumber("Enter a Number: ")
-    name1 = getWord ("Enter a name")
-    noun1 = getWord ("Enter a noun")
-    time = getNumber ("Enter a time")
-    pronoun1 = getWord ("Enter a pronoun")
-    adajetive = getWord ("Enter a adajetive")
-    restrontName =getWord ("Enter a restrontName")
-    city = getWord ("Enter a city")
-    pronoun2 = getWord ("Enter a pronoun")
-    name2 = getWord ("Enter a name")
-    name3 = getWord ("Enter a name")
-    name4 = getWord ("Enter a name")
-    name5 = getWord ("Enter a name")
+    Location1 = getWord( "Enter a location: ")
+    Temperature1 = getNumber( "Enter a Number: ")
+    Name1 = getWord ( "Enter a name: " )
+    Noun1 = getWord ( "Enter a noun: " )
+    Time = getNumber ( "Enter a time: " )
+    Pronoun1 = getWord ( "Enter a pronoun: " )
+    Adajetive1 = getWord ( "Enter a adajetive:" )
+    Adajetive2 = getWord ( "Enter a adajetive:" )
+    RestrontName = getWord ( "Enter a restrontName: " )
+    City = getWord ( "Enter a city:" )
+    Pronoun2 = getWord ( "Enter a pronoun: " )
+    Name2 = getWord ( "Enter a name: " )
+    Name3 = getWord ( "Enter a name: " )
+    Noun1 = getWord ( "Enter a noun: " )
+    Noun2 = getWord ( "Enter a noun: " )
+    Noun3 = getWord ( "Enter a noun: " )
+    Noun4 = getWord ( "Enter a noun: " )
+    Noun5 = getWord ( "Enter a noun: " )
+    Verb1 = getWord ( "Enter a noun: " )
+   
      
     text = ""
-    text += "One day I went to the " + location1
-    text += ". It was like a " + temperature1
+    text += "One day I went to the " + Location1
+    text += ". It was like a " + Temperature1
     text += " out."
-    text += " I looked for my friend " + Name1 
-    text += " s/he was always later so I " + Noun1 
-    text += " it was " + time + " when s/he arived " 
-    text += " I asked " + Name2 + " what took him/her " + Noun2
-    text += " s/he said " + Name + " was talking to me about" + pronoun1
-    text += " I looked at her/him and thought of " + Noun3
-    text += " but then " + name + " walked up to me and asked me if I was ok I said" + adajetive
-    text += " I then sais we shoud go to " + restrontName + " for lunch "
-    text += " I know how much " + Name + " loved there" + Food
-    text += " we starwd walking to " + restront Name 
-    text += " we stard taking about " + Noun2
-    text += " after some time past we made it to the restront I looked at my wach to see it wat " + time 
-    text += " I then remaber that I had to pick up my sister " + Name3 
-    text += " she was just getting back from " + city
-    text += " her plane was landing at " + time 
-    text += " I see that I have time to eat with my friend " + Name 
-    text += " then we see a friend from work " +Name 
-    text += "S/he wlked over to use and asked us about " + pronoun
-    text += " 
+    text += " I looked for my friend " + Name1 + 
+    text += " s/he was always later so I " + Noun1 + 
+    text += " it was " + Time1 + " when s/he arived " 
+    text += " I asked " + Name1 + " what took him/her so " + Noun2 +
+    text += " s/he said " + Name2 + " was talking to S/he about" + Pronoun1 +
+    text += " I looked at her/him and thought of " + Noun3 +
+    text += " but then " + Name1 + " walked up to me and asked me if I was ok I said" + Adajetive1 +
+    text += " I then said we shoud go to " + RestrontName1 + " for lunch "
+    text += " I know how much " + Name1 + " loved there" + Food +
+    text += " we starwd walking to " + RestrontName1 +
+    text += " we stard taking about " + Noun4 +
+    text += " after some time past we made it to the restront I looked at my wach to see it wat " + Time2 + 
+    text += " I then remaber that I had to pick up my sister " + Name3 + 
+    text += " she was just getting back from " + City +
+    text += " her plane was landing at " + Time3 + 
+    text += " I see that I have time to eat with my friend " + Name1 + 
+    text += " then we see a friend from work " + Name4 +
+    text += "S/he wlked over to use and asked us about " + Pronoun2 +
+    text += " I look at S/he lost I don't know what to say so I ask " + Noun5 +
+    text += " We all just sand in silens it feels so " + Verb1 +
+    text += " Then the waiter walks up to the table and ask " + Adajetive2 +
     
     +
     return text
