f1fed44c4cda1f80c812c49350710575c2a7865a
matt <matthew.disalvo@kearsarge.org>
2016-10-13 10:52:30 -0400

    Added more

--- story2.py
+++ story2.py
@@ -2,11 +2,26 @@ from input import *
 
 #Written by Matthew 
 def story():
-	location1 = getWorld("Enter a Location: ")
-	temperaturel = getNumber ("Enter a Number: ")
+	time = getNumber("Enter the time: ")
+	talk = getTalk("Enter what you say: ")
+	verb1 = getWord("Type in a verb: ")
+	noun1 = getWord("Type in a  noun: ")
+	verb2 = getWord("Type in a verb: ")
+	verb3 = getWord("Type in a verb: ")
+	noun2 = getWord("Type in a noun: ")
+	verb4 = getWord("Type in a noun: ")
 	
 	text = ""
-	text =+ "You see a clown walking on the road."
+	text += "The time is " + time
+	text += ". You say " + talk
+	text += ". You see a clown walking on the road, you " + verb1
+	text += ". The clown sees you coming towards him"
+	text += ". The clown looks inside his bag a grabs out a " + noun1
+	text += ". You decided to " + verb2
+	text += ". The clown starts to " + verb3
+	text += ". You look around and find a " + noun2
+	text += ". The clown begins to " + verb4
+	
 
 	
 	
