9d37848e1f224cb3eb2b4bc2d68f683c36e15dcb
Felicia Merrill <Felicia.Merrill@kearsarge.org>
2016-10-13 10:55:00 -0400

    Add to story 1

--- story1.py
+++ story1.py
@@ -4,51 +4,56 @@ from input import *
 def story():
     Location1 = getWord( "Enter a location: ")
     Temperature1 = getNumber( "Enter a Number: ")
-    Name1 = getWord ( "Enter a name: " )
-    Noun1 = getWord ( "Enter a noun: " )
-    Time = getNumber ( "Enter a time: " )
-    Pronoun1 = getWord ( "Enter a pronoun: " )
-    Adajetive1 = getWord ( "Enter a adajetive:" )
-    Adajetive2 = getWord ( "Enter a adajetive:" )
     RestrontName = getWord ( "Enter a restrontName: " )
     City = getWord ( "Enter a city:" )
-    Pronoun2 = getWord ( "Enter a pronoun: " )
+    Time1 = getNumber ( "Enter a time: " )
+    Time2 = getNumber ( "Enter a time: " )
+    Time3 = getNumber ( "Enter a time: " )
+    Name1 = getWord ( "Enter a name: " )
     Name2 = getWord ( "Enter a name: " )
     Name3 = getWord ( "Enter a name: " )
+    Adajetive1 = getWord ( "Enter a adajetive:" )
+    Adajetive2 = getWord ( "Enter a adajetive:" )
     Noun1 = getWord ( "Enter a noun: " )
     Noun2 = getWord ( "Enter a noun: " )
     Noun3 = getWord ( "Enter a noun: " )
     Noun4 = getWord ( "Enter a noun: " )
     Noun5 = getWord ( "Enter a noun: " )
     Verb1 = getWord ( "Enter a noun: " )
-   
-     
+    Pronoun1 = getWord ( "Enter a pronoun: " )
+    Pronoun2 = getWord ( "Enter a pronoun: " )
+    Food1 = getword ( "Enter a Food: " )
+    Food2 = getword ( "Enter a Food: " )
+    BodyMoveMent = getword ( "Enter BodyMoveMent: " )
+    
+    
     text = ""
     text += "One day I went to the " + Location1
     text += ". It was like a " + Temperature1
     text += " out."
-    text += " I looked for my friend " + Name1 + 
-    text += " s/he was always later so I " + Noun1 + 
-    text += " it was " + Time1 + " when s/he arived " 
-    text += " I asked " + Name1 + " what took him/her so " + Noun2 +
-    text += " s/he said " + Name2 + " was talking to S/he about" + Pronoun1 +
-    text += " I looked at her/him and thought of " + Noun3 +
-    text += " but then " + Name1 + " walked up to me and asked me if I was ok I said" + Adajetive1 +
-    text += " I then said we shoud go to " + RestrontName1 + " for lunch "
-    text += " I know how much " + Name1 + " loved there" + Food +
-    text += " we starwd walking to " + RestrontName1 +
-    text += " we stard taking about " + Noun4 +
-    text += " after some time past we made it to the restront I looked at my wach to see it wat " + Time2 + 
-    text += " I then remaber that I had to pick up my sister " + Name3 + 
-    text += " she was just getting back from " + City +
-    text += " her plane was landing at " + Time3 + 
-    text += " I see that I have time to eat with my friend " + Name1 + 
+    text += " I looked for my friend " + Name1  
+    text += " s/he was always later so I " + Noun1 
+    text += " it was " + Time1  " when s/he arived. I looked at her/him and " + BodyMoveMent
+    text += " I asked " + Name1 " what took him/her so " + Noun2 
+    text += " s/he said " + Name2  " was talking to S/he about" + Pronoun1 
+    text += " I looked at her/him and thought of " + Noun3 
+    text += " but then " + Name1  " walked up to me and asked me if I was ok I said" + Adajetive1 
+    text += " I then said we shoud go to " + RestrontName1  " for lunch "
+    text += " I know how much " + Name1  " loved there" + Food1 
+    text += " we starwd walking to " + RestrontName1 
+    text += " we stard taking about " + Noun4 
+    text += " after some time past we made it to the restront I looked at my wach to see it wat " + Time2  
+    text += " I then remaber that I had to pick up my sister " + Name3  
+    text += " she was just getting back from " + City 
+    text += " her plane was landing at " + Time3 
+    text += " I see that I have time to eat with my friend " + Name1 
     text += " then we see a friend from work " + Name4 +
-    text += "S/he wlked over to use and asked us about " + Pronoun2 +
-    text += " I look at S/he lost I don't know what to say so I ask " + Noun5 +
-    text += " We all just sand in silens it feels so " + Verb1 +
-    text += " Then the waiter walks up to the table and ask " + Adajetive2 +
-    
+    text += "S/he wlked over to use and asked us about " + Pronoun2 
+    text += " I look at S/he lost I don't know what to say so I ask " + Noun5 
+    text += " We all just sand in silens it feels so " + Verb1 
+    text += " Then the waiter walks up to the table and ask " + Adajetive2 
+    text += " I tell the waiter what I want then I order for my frien I get her/him " + Food2 
+    text += " 
     +
     return text
 
