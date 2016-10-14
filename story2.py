444429cf71946432a279a3028f9e11dacdebcdd4
matt <matthew.disalvo@kearsarge.org>
2016-10-14 11:17:51 -0400

    Added more

--- story2.py
+++ story2.py
@@ -6,22 +6,27 @@ def story():
 	talk = getTalk("Enter what you say: ")
 	verb1 = getWord("Type in a verb: ")
 	noun1 = getWord("Type in a  noun: ")
+	talk2 = getTalk("Enter what you say: ")
 	verb2 = getWord("Type in a verb: ")
 	verb3 = getWord("Type in a verb: ")
 	noun2 = getWord("Type in a noun: ")
 	verb4 = getWord("Type in a noun: ")
+	talk3 = getTalk("Enter what you say: ")
+	
+	
 	
 	text = ""
 	text += "The time is " + time
-	text += ". You say " + talk
+	text += ". You say " + talk1
 	text += ". You see a clown walking on the road, you " + verb1
 	text += ". The clown sees you coming towards him"
 	text += ". The clown looks inside his bag a grabs out a " + noun1
+	text += ". You think to yourself " + talk2
 	text += ". You decided to " + verb2
 	text += ". The clown starts to " + verb3
 	text += ". You look around and find a " + noun2
 	text += ". The clown begins to " + verb4
-	
+	text += ". The clown says " + talk3
 
 	
 	
