3f3ec0ad0e94d87c72a2e3a2f5e4aaa7792b8948
Tom <Thomas.mann@kearsarge.org>
2016-10-14 11:39:24 -0400

    add to story3

--- stroy3.py
+++ stroy3.py
@@ -8,13 +8,16 @@ def story():
     
     
     text = ""
-    text += "you get huger so you go get" + NameFoFood
-    text += " as you get in you see a girl" + Name
+    text += "you get hungry so you go get" + NameOfFood
+    text += " as you get into the car you see a girl" + Name
     text +="you and her talking adout" + Noun
-    text + "you ask her if she whans" +NameFoFood
-
-
-
-
-
+    text += "you ask her what would you like to eat? " + NameOfFood
+    text += " she says I wolud like" + NameOfFood 
+    text += "you and the girl go get food and talk adout" + noun
+    text += "you and the girl are at wallmart looking for" + food
+    text += "as you are looking for food you start to hear your favorit song" + NameOfMusic
+    text += " you and the girl start to go home and talk about" + noun
+    text +="Once you two get home, you both say goodbye to eachother while looking in eachothers eyes."
+    
+    
     return text
