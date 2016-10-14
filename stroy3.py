cd3470ead08e4c49e791a3f445bdd65015b54857
Tom <Thomas.mann@kearsarge.org>
2016-10-14 11:13:30 -0400

    making story 3

---
+++ stroy3.py
@@ -0,0 +1,20 @@
+form input import *
+
+#written by Tommy
+def story():
+    
+    
+    
+    
+    
+    text = ""
+    text += "you get huger so you go get" + NameFoFood
+    text += " as you get in you see a girl" + Name
+    text +="you and her talking adout" + Noun
+    text + "you ask her if she whans" +NameFoFood
+
+
+
+
+
+    return text
