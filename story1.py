89cb9a916d4547acaf687decd01f856b3af50a00
Felicia Merrill <Felicia.Merrill@kearsarge.org>
2016-10-17 10:59:28 -0400

    add to story one

--- story1.py
+++ story1.py
@@ -14,12 +14,14 @@ def story():
     Name3 = getWord ( "Enter a name: " )
     Adajetive1 = getWord ( "Enter a adajetive:" )
     Adajetive2 = getWord ( "Enter a adajetive:" )
+    Adajetive2 = getWord ( "Enter a adajetive:" )
     Noun1 = getWord ( "Enter a noun: " )
     Noun2 = getWord ( "Enter a noun: " )
     Noun3 = getWord ( "Enter a noun: " )
     Noun4 = getWord ( "Enter a noun: " )
     Noun5 = getWord ( "Enter a noun: " )
     Noun6 = getWord ( "Enter a noun: " )
+    Verb1 = getWord ( "Enter a noun: " )
     Verb1 = getWord ( "Enter a noun: " )
     Pronoun1 = getWord ( "Enter a pronoun: " )
     Pronoun2 = getWord ( "Enter a pronoun: " )
@@ -50,10 +52,14 @@ def story():
     text += " then we see a friend from work " + Name4 +
     text += "S/he wlked over to use and asked us about " + Pronoun2 
     text += " I look at S/he lost I don't know what to say so I ask " + Noun5 
-    text += " We all just sand in silens it feels so " + Noun6 
+    text += " We all just sand in silens it feels so " + Verb2 
     text += " Then the waiter walks up to the table and ask " + Adajetive2 
     text += " I tell the waiter what I want then I order for my frien I get her/him " + Food2 
-    text += " 
+    text += " I then turn back to my friend " + Name4 " To finsh talking anbout " + Noun6
+    text += " I then here someone yell my name so I " + Moivement 
+    text += " I see my sister waling towerds me I go to say hi but she staresd " + Adajetive3
+    text += " and that why I'm never late picking people up from " + place
+    text += " I never want that to hapen to me agan! "
     +
     return text
 
