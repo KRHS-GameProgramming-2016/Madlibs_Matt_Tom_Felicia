5ed8f3643d98b17fadca4dacea5863d8d7c4f09f
Tom <Thomas.mann@kearsarge.org>
2016-10-20 11:07:44 -0400

    add to story3

--- stroy3.py
+++ stroy3.py
@@ -3,14 +3,14 @@ form input import *
 #written by Tommy
 def story():
     
-    NameOfFood1=getNumber (" Enter NameoFFood")
-    NameOfFood2=getNumber (" Enter NameoFFoood")
-    NameOfFood3=getNumber (" Enter NameoFFoood")
+    NameOfFood1=getwords (" Enter NameoFFood")
+    NameOfFood2=getwords (" Enter NameoFFoood")
+    NameOfFood3=getwords (" Enter NameoFFoood")
     Name=getNumber (" Enter Name")
     Noun1=getNumber (" Enter Noun")
     Noun2=getNumber (" Enter Noun")
     Noun3=getNumber (" Enter Noun")
-    NameOfFood=getNumber (" Enter NameOfFood")
+    NameOfFood4=getwords (" Enter NameOfFood")
     food=getNumber (" Enter food")
     NameOfMusic=getNumber ("Enter NameOfMusic")
     
@@ -18,7 +18,7 @@ def story():
     text += "you get hungry so you go get" + NameOfFood1
     text += " as you get into the car you see a girl" + Name
     text +="you and her talking adout" + Noun1
-    text += "you ask her what would you like to eat? " + NameOfFood
+    text += "you ask her what would you like to eat? " + NameOfFood4
     text += " she says I wolud like" + NameOfFood3
     text += "you and the girl go get food and talk adout" + Noun2
     text += "you and the girl are at wallmart looking for" + food
